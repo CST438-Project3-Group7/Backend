b84fb69d9c158bac4af51cced1d4867e2cc7e0ba
Jashan Rahal <jashanrahal6@gmail.com>
2024-11-09 12:16:52 -0800

    added user registration endpoint

--- project3Backend/src/main/java/com/csumb/project3Backend/controllers/UserController.java
+++ project3Backend/src/main/java/com/csumb/project3Backend/controllers/UserController.java
@@ -73,4 +73,52 @@ public class UserController {
         }
 
     }
+
+    @PostMapping("/register")
+    public Map<String, Object> register(@RequestBody Map<String, Object> user) throws InterruptedException, ExecutionException{
+        //return user if a succesfull registration; Else returns an empty object
+
+        String username = (String) user.get("username");
+        String password = (String) user.get("password");
+        String role = "user";
+
+        //THIS SHOULD BE CHANGED TO A USER MODEL NOT A HASHMAP
+        Map<String, Object> newUser = new HashMap<>();
+        newUser.put("username", username);
+        newUser.put("password", password);
+        newUser.put("role", role);
+
+
+
+        CollectionReference usersRef = db.collection("users");
+
+        // Find the highest existing userID
+        ApiFuture<QuerySnapshot> query = usersRef.orderBy("userID", Query.Direction.DESCENDING).limit(1).get();
+        List<QueryDocumentSnapshot> documents = query.get().getDocuments();
+
+        int newUserId = 1; // Default to 1 if no users exist
+        if (!documents.isEmpty()) {
+            int maxUserId = documents.get(0).getLong("userID").intValue();
+            newUserId = maxUserId + 1; // Increment highest userID by 1
+        }
+
+        // Set the new userID
+        newUser.put("userID", newUserId);
+
+
+        // Add new user to DB
+        ApiFuture<DocumentReference> addedUserRef = usersRef.add(newUser);
+
+        try {
+            // Check if the user was added
+            DocumentReference documentRef = addedUserRef.get();
+            if (documentRef != null) {
+                return newUser;
+            }
+        } catch (Exception e) {
+            System.out.println("Error adding user: " + e.getMessage());
+        }
+
+        return new HashMap<>();
+    }
 }
