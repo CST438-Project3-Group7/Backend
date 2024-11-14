01d6b66b2680a5d11c96d32509b1a90230c87f9d
Jashan Rahal <jashanrahal6@gmail.com>
2024-11-14 00:08:03 -0800

    added & connected feedService for sorting/filtering

--- project3Backend/src/main/java/com/csumb/project3Backend/controllers/FeedController.java
+++ project3Backend/src/main/java/com/csumb/project3Backend/controllers/FeedController.java
@@ -1,6 +1,7 @@
 package com.csumb.project3Backend.controllers;
 import org.springframework.web.bind.annotation.*;
-
+import com.csumb.project3Backend.services.FeedService;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -16,11 +17,36 @@ import java.util.HashMap;
 
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/feed")
 public class FeedController {
 
+    @Autowired
+    private FeedService feedService;
 
-    // Put controller methods here
+    // returns all post sorted from last added to first added
+    @GetMapping("/all")
+    public String getAll(){
+        return feedService.getAll();
+    }
+
+    // return feed sorted by rating (highest -> lowest)
+    @GetMapping("/highestRating")
+    public String getHighestRating(@RequestBody int filterSetting){
+        return feedService.getHighestRating(filterSetting);
+    }
+
+    // return feed sorted by rating (lowest -> highest)
+    @GetMapping("/lowestRating")
+    public String getLowestRating(@RequestBody int filterSetting){
+        return feedService.getLowestRating(filterSetting);
+    }
+
+    // return feed sorted by subject (takes in the name of the subject they want to see)
+    @GetMapping("/subject")
+    public String getSubject(@RequestBody String subject){
+        return feedService.getSubject(subject);
+    }
+
 
 
 }
---
+++ project3Backend/src/main/java/com/csumb/project3Backend/services/FeedService.java
@@ -0,0 +1,43 @@
+package com.csumb.project3Backend.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * This class is used to define make queries for feed settings (ratings, subject, date)
+ * It should user the repository folder to call the database
+ */
+
+
+@Service
+public class FeedService {
+
+//USED FOR WHEN WE ADD DB & REPO FOLDER
+//    @Autowired
+//    private FeedRepo feedRepo
+
+    public String getAll(){
+        return "All Posts";
+    }
+
+    public String getHighestRating(int filterSetting){
+        return "feed sorted highest -> lowest";
+    }
+    public String getLowestRating(int filterSetting){
+        return "feed sorted lowest -> highest";
+    }
+
+    public String getSubject(String subject){
+        if(subject.equals("games")){
+            return "games";
+        }else if (subject.equals("music")){
+            return "music";
+        } else if (subject.equals("books")){
+            return "books";
+        }else{
+            return "movies";
+        }
+    }
+
+
+}
